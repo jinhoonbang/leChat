e4a146490ee9ea2c9af87a3f63e5cbcda7f04871
An <30442559+epaqu@users.noreply.github.com>
2017-12-28 02:15:32 +0900

    added some new features

--- scraper.R
+++ scraper.R
@@ -1,14 +1,16 @@
-#Setting two vectors for actor names and their ManNo's
-names <- "Name"
-ids <- "ID"
+library(XML)
+library(plyr)
 
-#how many pages or iterations needed
+#Setting an empty data frame
+output <- {}
+
+#see how many iterations are needed
 url <- "http://www.playdb.co.kr/artistdb/list_iframe.asp?Page=1&code=013003&NameSort=Y&Country=Y"
 web_data <- readLines(url, warn=FALSE)
 web_data <- paste(web_data, collapse = ' ')
 pages <- as.numeric(substr(web_data, regexpr("total 1/", web_data)[[1]]+8, regexpr("]</span>", web_data)[[1]]-1))
 
-#scraping
+#scrape
 for (page in 1:pages)
 {
   url <- paste0("http://www.playdb.co.kr/artistdb/list_iframe.asp?Page=", page, "&code=013003&NameSort=Y&Country=Y")
@@ -21,18 +23,26 @@ for (page in 1:pages)
   
   for (i in 1: (length(id_starting_indices)/2))
   {
-    #get ManNo's
+    #get id
     id <- substr(web_data, id_starting_indices[i*2], id_ending_indices[i*2])
     
-    #get names
+    #get name
     name <- substr(web_data, name_starting_indices[i*2], name_ending_indices[i*2])
     name <- substr(name, 1, regexpr("<", name)-1)
     
+    #get works
+		url <- paste0("http://www.playdb.co.kr/artistdb/Detail_Content_new.asp?TabKind=3&ManNo=", id)
+		history <- iconv(paste0(readLines(url, encoding="CP949"), collapse="\n"), to="UTF-8")
+		musicals <- xpathSApply(htmlParse(history, isHTML=T, asText=T, encoding="UTF-8"), "//td[@class='ptitle']", xmlValue)
+		dates <- xpathSApply(htmlParse(history, isHTML=T, asText=T, encoding="UTF-8"), "//td[@class='time']", xmlValue)
+    
+    #format data
+		block <- cbind(id, name, musicals, dates)
+
     #append
-    ids <- append(ids, id)
-    names <- append(names, name)
+		output <- rbind(output, block)
   }
 }
 
 #save the output
-write.csv(as.data.frame(cbind(ids, names)), "data.csv")
+write.csv(output, "data.csv")
