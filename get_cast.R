3fb5f5efc0cd3951c70732a942242a8761aba215
An <30442559+epaqu@users.noreply.github.com>
2017-12-28 17:43:30 +0900

    added some comments

--- get_cast.R
+++ get_cast.R
@@ -1,10 +1,17 @@
 library(XML)
 library(plyr)
 
+#concatenate musical titles and dates
 Musical <- paste0(as.character(output$musicals), "(", as.character(output$dates), ")")
+
+#concatenate artist name and id
 Artist <- paste0(as.character(output$name), "(", as.character(output$id), ")")
+
+#combine the two vectors into a data frame.
 sorted <- cbind(Musical, Artist)
 sorted <- as.data.frame(sorted)
+
+#sort the data frame by Musical
 sorted <- sorted[order(sorted$Musical),]
 result <- {}
 
@@ -12,6 +19,8 @@ i <- 1
 while (i < dim(sorted)[1])
 {
   j <- i+1
+  
+  #row = musical, actor1, actor2, actor3, ..., actorN
   row <- as.character(sorted[i,1])
   row <- append(row, as.character(sorted[i,2]))
   while (sorted[j,1] == sorted[i,1])
@@ -20,6 +29,8 @@ while (i < dim(sorted)[1])
     j <- j+1
   }
   i <- i + length(row)-1
+  
+  #save each row in result
   result <- rbind.fill(as.data.frame(result), as.data.frame(t(row)))
 }
 
