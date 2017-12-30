b52ec4d6561d561fdbb26f2a438cbf46f77eba69
An <30442559+epaqu@users.noreply.github.com>
2017-12-30 15:38:45 +0900

    added a few new features & fixed some issues
    Using a nested for loop to get scores took too much time, so I separated the loops.

--- scraper.R
+++ scraper.R
@@ -1,5 +1,6 @@
 library(XML)
 library(plyr)
+library(rvest)
 
 #Setting an empty data frame
 output <- {}
@@ -10,7 +11,7 @@ web_data <- readLines(url, warn=FALSE)
 web_data <- paste(web_data, collapse = ' ')
 pages <- as.numeric(substr(web_data, regexpr("total 1/", web_data)[[1]]+8, regexpr("]</span>", web_data)[[1]]-1))
 
-#scrape
+#main scraper
 for (page in 1:pages)
 {
 	url <- paste0("http://www.playdb.co.kr/artistdb/list_iframe.asp?Page=", page, "&code=013003&NameSort=Y&Country=Y")
@@ -20,7 +21,7 @@ for (page in 1:pages)
 	id_ending_indices <- as.vector(gregexpr("&part=013003", web_data)[[1]])-1 #even only
 	name_starting_indices <- as.vector(gregexpr("top\">", web_data)[[1]])+5 #even only
 	name_ending_indices <- name_starting_indices+10 #even only
-  
+
 	for (i in 1: (length(id_starting_indices)/2))
 	{
 		#get id
@@ -36,32 +37,62 @@ for (page in 1:pages)
 		musicals <- xpathSApply(htmlParse(history, isHTML=T, asText=T, encoding="UTF-8"), "//td[@class='ptitle']", xmlValue)
 		dates <- xpathSApply(htmlParse(history, isHTML=T, asText=T, encoding="UTF-8"), "//td[@class='time']", xmlValue)
 
-		#get workcodes
-		work_data <- readLines(url, warn=FALSE)
-		work_data <- paste(work_data, collapse = ' ')
-		code_starting_indices <- as.vector(gregexpr("dbDetail.asp?", work_data)[[1]])+24 #even only
-		code_ending_indices <- as.vector(gregexpr("target=\"_parent\"><", work_data)[[1]])-3
-		codes <- {}
-		scores <- {}
-		for (k in 1:(length(code_starting_indices)/2))
-		{
-			code <- substr(work_data, code_starting_indices[k*2-1], code_ending_indices[k])
-			codes <- append(codes, code)
-			
-			#get scores
-			url <- paste0("http://www.playdb.co.kr/playdb/playdbDetail.asp?sReqPlayno=", code)
-			score <- iconv(paste0(readLines(url, encoding="CP949", warn=FALSE), collapse="\n"), to="UTF-8")
-			score <- xpathSApply(htmlParse(score, isHTML=T, asText=T, encoding="UTF-8"), "//dd[@class='score']", xmlValue)
-			scores <- append(scores, score[1])
-		}
-
 		#format data
-		block <- cbind(id, name, musicals, codes, dates, scores)
+		block <- cbind(id, name, musicals, dates)
 		
 		#append
 		output <- rbind.fill(as.data.frame(output), as.data.frame(block))
-  }
+	}
 }
 
+#scraping playNo (musical id)
+codes <- {}
+ids <- unique(output$id)
+for (persons in 1:length(ids))
+{
+	#get workcodes
+	scores <- {}
+	url <- paste0("http://www.playdb.co.kr/artistdb/Detail_Content_new.asp?TabKind=3&ManNo=", ids[persons])
+	web_data <- readLines(url, warn=FALSE)
+	web_data <- paste(web_data, collapse = ' ')
+	code_starting_indices <- as.vector(gregexpr("dbDetail.asp?", web_data)[[1]])+24 #even only
+	code_ending_indices <- as.vector(gregexpr("target=\"_parent\"><", web_data)[[1]])-3
+	for (k in 1: (length(code_starting_indices)/2))
+	{
+		code <- substr(web_data, code_starting_indices[k*2-1], code_ending_indices[k])
+		codes <- append(codes, code)
+	}
+}
+codes[which(codes=="")] <- NA
+
+#scraping scores (out of 10) by audience
+scores <- {}
+for (cd in codes)
+{
+	#get scores
+	url <- paste0("http://www.playdb.co.kr/playdb/playdbDetail.asp?sReqPlayno=", cd)
+	url_html <-  read_html(url, encoding="CP949")
+	score_node <- html_node(url_html, "dd.score")
+	score <- as.character(html_text(score_node))
+	scores <- append(scores, as.character(score))
+}
+
+#replace empty cells with NA
+scores[which(scores=="")] <- NA
+
+#combine and omit all rows with NA
+output_final <- cbind(output, codes, scores)
+output_final <- na.omit(output_final)
+
+#strsplit dates column into two (start & end)
+time_i <- {}
+time_f <- {}
+for (date in output_final$dates)
+{
+    time_i <- append(time_i, strsplit(date, " ~ ")[[1]][1])
+    time_f <- append(time_f, strsplit(date, " ~ ")[[1]][2])
+}
+output_final <- cbind(output_final, time_i, time_f)
+
 #save the output
-write.csv(output, "data.csv")
+write.csv(output_final, "data.csv")
